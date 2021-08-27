8dca09aec033a7b712ff57765bf30e7ef89197b2
High_Lander <codywmarkham@gmail.com>
2021-08-26 21:57:17 -0500

    changed secret from env

--- server.js
+++ server.js
@@ -5,14 +5,18 @@ const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars')
 const session = require('express-session')
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-    secret: process.env.DB_SESSION_SECRET,
-    cookie: { maxAge: 7200000 },
+    secret: 'Super secret secret',
+    cookie: {},
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
@@ -20,21 +24,18 @@ const sess = {
     })
   };
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use(session(sess));
+  app.use(session(sess));
+  
+  app.engine('handlebars', hbs.engine);
+  app.set('view engine', 'handlebars');
+  
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static(path.join(__dirname, 'public')));
+  
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
   });
